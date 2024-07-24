92014d11d7a0182ef84fa014d7ba81a990df6247
RaduTek <me@radutek.uk>
2024-07-24 12:01:45 +0300

    Create base types for BIOS

---
+++ bios.py
@@ -0,0 +1,34 @@
+from typing import List, Callable, Any, NotRequired, TypedDict
+
+
+class Item(TypedDict):
+    title: str
+    type: NotRequired[str]
+    help: NotRequired[str]
+    value: NotRequired[Any]
+    values: NotRequired[List[Any]]
+    function: NotRequired[Callable]
+
+
+class Page(TypedDict):
+    title: str
+    items: List[Item]
+
+
+def get_selectable_index(items: List[Item], offset: int = -1, reverse: bool = False):
+    r = range(offset - 1, -1, -1) if reverse else range(offset + 1, len(items))
+    for i in r:
+        item = items[i]
+        if item:
+            return i
+    return offset
+
+
+PageGenerators = List[Callable]
+
+
+def new_page_generator(page: Page) -> Callable:
+    def generator():
+        return page
+
+    return generator
